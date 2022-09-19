73ecd1cd16bbd098007aff0e9bf5af5a61a43d18
elias-brownlee <113552481+elias-brownlee@users.noreply.github.com>
2022-09-19 10:33:34 -0500

    update

--- src/main/MortgageCalculator.java
+++ src/main/MortgageCalculator.java
@@ -42,6 +42,6 @@ public class MortgageCalculator {
 
         MortgageCalculator calculator = new MortgageCalculator(loanAmount, termInYears, annualRate);
         calculator.calculateMonthlyPayment();
-        System.out.println(calculator.toString())
+        System.out.println(calculator.toString());
     }
 }
