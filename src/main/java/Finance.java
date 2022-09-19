5f020312569cd501e11e91642c96f04be644a94d
elias-brownlee <113552481+elias-brownlee@users.noreply.github.com>
2022-09-19 11:08:10 -0500

    update

--- src/main/java/Finance.java
+++ src/main/java/Finance.java
@@ -13,7 +13,7 @@ public class Finance {
     public static final Map<String, String> commandsToUsage
     = Map.of(
         BEST_LOAN_RATES, "usage: bestLoanRates",
-        SAVINGS_CALCULATOR, "usage: savingsCalculator <credits seperated by ','> <debits seperated by ','>",
+        SAVINGS_CALCULATOR, "usage: savingsCalculator <credits separated by ','> <debits separated by ','>",
         MORTGAGE_CALCULATOR, "usage: mortgageCalculator <loanAmount> <termInYears> <annualRate>"
     );
 
